7d82d2db9f5686c20b1f15d7548a6fbf48aba794
Protix <azadseker@hotmail.com>
2023-01-30 09:07:05 +0100

    Correcting error
    Error corrected in file

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,7 +4,7 @@ var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_left")
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
 	if input_vector != Vector2.ZERO:
