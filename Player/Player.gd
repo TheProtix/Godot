9239091b1e8e8b488dd99c0b0600144bd1db9dd9
Protix <azadseker@hotmail.com>
2023-01-30 09:12:35 +0100

    Test

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
+	#Handles user input
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
